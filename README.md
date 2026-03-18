8c1c4d110e343346ef1be8a5d755b3aa766b20c4
san2794 <sandeepthi.21bce9647@vitapstudent.ac.in>
2026-03-18 17:29:50 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # task8-sqs
+
+Task 8 - AWS CDK project for sending and receiving messages using Amazon SQS.
---
+++ test/task8-sqs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Task8Sqs from '../lib/task8-sqs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/task8-sqs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Task8Sqs.Task8SqsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
